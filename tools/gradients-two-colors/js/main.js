6ad520b81c58b95b167675ac4601ffbe39d15f39
Kostas <kostas.sliazas@gmail.com>
2023-10-08 08:58:14 +0300

    Add files via upload

--- tools/gradients-two-colors/js/main.js
+++ tools/gradients-two-colors/js/main.js
@@ -1,68 +1,68 @@
-;(function () {
-  const body = document.body
-  const colorDirection = document.getElementsByName('direction')
-  const color1 = document.getElementById('color1')
-  const color2 = document.getElementById('color2')
-  const colorOut = document.getElementById('color-out')
-  const copyTxt = document.getElementById('copy-text')
-  const ranges = document.getElementById('ranges')
-  const wrp = document.getElementById('wraoer')
-  const toggleHideClass = document.getElementById('toggle')
-  let range
+/*jshint esversion:11*/
+(function () {
+  const body = document.body;
+  const colorDirection = document.getElementsByName("direction");
+  const color1 = document.getElementById("color1");
+  const color2 = document.getElementById("color2");
+  const colorOut = document.getElementById("color-out");
+  const copyTxt = document.getElementById("copy-text");
+  const ranges = document.getElementById("ranges");
+  const wrp = document.getElementById("wrapper");
+  let range;
   // get checked value
-  function getCheckedRadio () {
-    let checked
-    colorDirection.forEach(element => {
-      if (element.checked) checked = element.value
-    })
-    return checked
+  function getCheckedRadio() {
+    let checked;
+    colorDirection.forEach((element) => {
+      if (element.checked) checked = element.value;
+    });
+    return checked;
   }
-  // copy input text to cliboard
-  function copyText (element) {
-    element.select()
-    element.setSelectionRange(0, 99999)
-    document.execCommand('copy')
+  // copy input text to clipboard
+  function copyText(element) {
+    element.select();
+    element.setSelectionRange(0, 99999);
+    document.execCommand("copy");
   }
 
   // update all stuff
-  function updateValues (e) {
-    getCheckedRadio()
-    setLabel()
-    range = (e && e.target.parentElement.className === 'radios') ? getCheckedRadio() : ranges.value + 'deg'
-    setBodyBackground()
-    colorOut.value = 'background:' + setGradient() + ';'
+  function updateValues(e) {
+    getCheckedRadio();
+    setLabel();
+    range = e && e.target.parentElement.className === "radios" ? getCheckedRadio() : ranges.value;
+    setBodyBackground();
+    colorOut.value = "background:" + setGradient() + ";";
   }
 
   // set gradient string
-  function setGradient () {
-    return `linear-gradient(${range}, ${color1.value}, ${color2.value})`
+  function setGradient() {
+    if (document.getElementById("linear").checked) return `linear-gradient(${range}deg, ${color1.value}, ${color2.value})`;
+    else return `radial-gradient(circle, ${color1.value}, ${color2.value})`;
   }
 
   // set body background
-  function setBodyBackground () {
-    body.style.backgroundImage = setGradient()
+  function setBodyBackground() {
+    body.style.backgroundImage = setGradient();
   }
 
   // set label of ranges
-  function setLabel () {
-    ranges.nextElementSibling.innerText = ranges.value + 'deg'
+  function setLabel() {
+    ranges.nextElementSibling.innerText = ranges.value + "deg";
   }
 
   // toggle classList hide
-  function classToggle (e) {
-    if (e.isComposing || e.key === 229) return
+  function classToggle(e) {
+    if (e.isComposing || e.key === 229) return;
     // event key or target id
-    console.log('ddddd')
-    // if(e.key === 'k') 
-    wrp.classList.toggle('hide')
-    e.preventDefault()
-    e.stopImmediatePropagation()
+    var keyCode = e.which;
+    if (keyCode == 72) {
+      wrp.classList.toggle("hide");
+      e.preventDefault();
+      e.stopImmediatePropagation();
+    }
   }
 
-  window.addEventListener('keyup', classToggle)
-  toggleHideClass.addEventListener('click', classToggle)
-
-  wrp.addEventListener('input', updateValues)
-  copyTxt.addEventListener('click', () => copyText(colorOut))
-  updateValues()
-})()
+  window.addEventListener("keyup", classToggle);
+  wrp.addEventListener("input", updateValues);
+  copyTxt.addEventListener("click", () => copyText(colorOut));
+  updateValues();
+})();
