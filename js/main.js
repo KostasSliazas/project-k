5532644f147f10a2b9e68e8eeaea73e34482a88b
Kostas <kostas.sliazas@gmail.com>
2023-12-09 16:06:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,38 +4,88 @@
  */
 /*jshint esversion: 11 */
 (function () {
-  "use strict";
+    "use strict";
+  // movable blocks default string
+  const blockDefaults = "width:140px;height:60px;left:290px;top:10px;,width:180px;height:60px;left:10px;top:10px;,width:100px;height:60px;left:190px;top:10px;,width:80px;height:60px;left:470px;top:50px;,width:80px;height:60px;left:470px;top:110px;,width:140px;height:40px;left:220px;top:450px;,width:190px;height:140px;left:360px;top:430px;,width:220px;height:40px;left:200px;top:170px;,width:210px;height:100px;left:10px;top:70px;,width:130px;height:40px;left:420px;top:170px;,width:190px;height:160px;left:360px;top:270px;,width:190px;height:60px;left:360px;top:210px;,width:250px;height:50px;left:220px;top:70px;,width:250px;height:50px;left:220px;top:120px;,width:20px;height:120px;left:200px;top:450px;,width:160px;height:240px;left:200px;top:210px;,width:140px;height:80px;left:220px;top:490px;,width:190px;height:350px;left:10px;top:220px;,width:160px;height:260px;left:590px;top:70px;,width:190px;height:50px;left:10px;top:170px;,width:120px;height:60px;left:430px;top:10px;"
+  // movable blocks defaults string
+  const textAreaDefaults = "Good day. How may I assist you? You have the ability to reposition these blocks by selecting and holding the left corner at your desired location, or double-click to minimize them. Additionally, you can customize the theme, colors, and background image to your liking. You are free to tailor this interface to your preferences. Alternatively, you may opt to close this window and continue with your activities. The decision is entirely yours. Wishing you a splendid day, and a fulfilling life."
+// https://stackoverflow.com/questions/45071353/copy-text-string-on-click/53977796#53977796
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');  // Create a <textarea> element
+  el.value = str;                                 // Set its value to the string that you want copied
+  el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+  document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+  const selected =
+    document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0)     // Store selection if found
+      : false;                                    // Mark as false to know no selection existed before
+  el.select();                                    // Select the <textarea> content
+  document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el);                  // Remove the <textarea> element
+  if (selected) {                                 // If a selection existed before copying
+    document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+    document.getSelection().addRange(selected);   // Restore the original selection
+  }
+};
 
   // prevent from fast accidental clicks
   const getElms = [...document.getElementsByTagName("button"), ...document.getElementsByTagName("a")];
 
-  // function loader(){
-  // console.log(this)
+
+  // let clicked = false;
+  // async function buttonClicker(e) {
+  //   if (clicked === true) {
+  //     e.preventDefault();
+  //     e.stopImmediatePropagation();
+  //     return;
+  //   }
+  //   clicked = true;
+  //   // await loader.call(e)
+  //   await delay(500);
+  //   clicked = false;
   // }
+  // getElms.forEach((e) => e.addEventListener("click", (e) => buttonClicker.call(null, e)));
+  // prevent from fast accidental clicks ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-  let clicked = false;
 
-  async function buttonClicker(e) {
-    if (clicked === true) {
+// for stoping acidental clicks
+  class ClickHandler {
+  constructor() {
+    this.clicked = false;
+  }
+
+  async buttonClicker(e) {
+    if (this.clicked === true) {
       e.preventDefault();
       e.stopImmediatePropagation();
       return;
     }
-
-    clicked = true;
-    // await loader.call(e)
-    await delay(500);
-    clicked = false;
+    this.clicked = true;
+    await this.delay(500);
+    this.clicked = false;
   }
+  delay(ms) { return new Promise(resolve => setTimeout(resolve, ms));}
+}
 
-  getElms.forEach((e) => e.addEventListener("click", (e) => buttonClicker.call(null, e)));
-  // prevent from fast accidental clicks ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+const clickHandler = new ClickHandler();
+getElms.forEach(element => element.addEventListener("click", e => clickHandler.buttonClicker(e)));
 
-  /**
-   * Local storage get item
-   *
-   * @returns {*}
-   */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   const getValueOfStorage = function () {
     return JSON.parse(localStorage.getItem(this));
   };
@@ -159,22 +209,40 @@
         }
       });
 
-      e.addEventListener("mousedown", (e) => {
-        //set global target element
-        if (e.target.classList.contains("movable")) {
-          if (target !== null) target.style.zIndex = 1;
-          target = e.target;
-          target.style.zIndex = 2;
-          moving = true;
-          e.target.classList.add("mousedown");
+     function findParent(element, className) {
+      let currentElement = element;
+
+    // Continue traversing up the DOM hierarchy until the body element
+    while (currentElement && !currentElement.classList.contains(className)) {
+        currentElement = currentElement.parentNode;
+
+        // Break the loop if the body element is reached (or no parent is found)
+        if (currentElement.tagName.toLowerCase() === 'body') {
+            return null;
         }
+    }
+
+    // Return the found parent element (or null if not found)
+    return currentElement;
+      }
+
+      e.addEventListener("mousedown", async function(e){
+          if(e.target === this){ moving = true; }
+
+          if (target !== null) target.style.zIndex = 'auto';
+          target = this;
+          target.style.zIndex = 1;
+
+          await delay(200)
+          if(moving)
+          this.classList.add("mousedown");
       });
     });
   };
 
   document.addEventListener("mouseup", () => {
-    if (target !== null && target.classList.contains("movable")) {
-      moving = false;
+    moving = false;
+    if (target) {
       setStyles();
       target.classList.remove("mousedown");
     }
@@ -183,28 +251,42 @@
     if (target !== null && target.classList.contains("movable") && moving) mouseMoves(z, target);
   });
 
-  const CL = {
-    ":": "cols",
-  };
 
-  function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    // s = checkTime(s);
-    h = checkTime(h);
-    let string = h + ":" + m; // + ":" + s;
-    let dk = [...string].map((e) => `<div class="digit ${CL[e]}"> ${e} </div>`).join("");
-    document.getElementById("clock").innerHTML = dk;
-    var t = setTimeout(startTime, 1000);
+
+
+
+  class Clock {
+  constructor(clockElementId) {
+    this.clockElement = document.getElementById(clockElementId);
   }
 
-  function checkTime(i) {
-    if (i < 10) i = "0" + i;
-    return i;
+  formatTimeUnit(unit) {
+    return unit < 10 ? `0${unit}` : unit;
   }
+
+  getCurrentTime() {
+    const day = new Date();
+    return {
+      m: this.formatTimeUnit(day.getMinutes()),
+      s: this.formatTimeUnit(day.getSeconds()),
+      h: this.formatTimeUnit(day.getHours()),
+    };
+  }
+
+  updateClock() {
+    const { h, m, s } = this.getCurrentTime();
+    this.clockElement.textContent = [h, m, s].join(':');
+  }
+
+  startTime() {
+    this.updateClock();
+    setInterval(() => this.updateClock(), 1000);
+  }
+}
+
+
+
+
 
   function showDate() {
     let d = new Date();
@@ -237,44 +319,53 @@
     localStorage.setItem("statsData", JSON.stringify(data.hourly.temperature_2m));
   }
 
+function reduceValuesDynamically(arr, max) {
+  if (arr.length === 0) {
+    return arr; // Return an empty array if input array is empty
+  }
+
+  const maxInArray = Math.max(...arr.map(value => Math.abs(value)));
+  const percentage = maxInArray > max ? ((maxInArray - max) / maxInArray) * 100 : 0;
+
+  const factor = 1 - percentage / 100;
+  const resultArray = arr.map(value => Math.min(value * factor, max));
+
+  return resultArray;
+}
+
   //stats
   function stats(data) {
     const stats = document.querySelector("#stats");
 
     if (!data) return (stats.innerText = "???");
-
-    const randomData = data;
-    // randomData.reverse()
     const main = document.querySelector(".svg-holder");
-    //show last 33
-    if (randomData.length > 32) randomData.length = 32;
+    // data = data.splice(-25, 25);
+    data.length = 25;
+    // data.reverse()
+    const arrayConverted = reduceValuesDynamically(data.map(e=>e.toFixed(2) * 10), 20);
 
-    let total = 0;
-    for (let i = 0; i < randomData.length; i++) total += randomData[i];
-    let avg = total / randomData.length;
-
-    const maxValue = Math.max(...randomData);
-    const arrayConverted = [];
-    for (let index = 0; index < randomData.length; ++index) arrayConverted.push(((randomData[index] + avg) * 100) / (maxValue + avg));
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("version", "1.1");
     svg.setAttribute("viewBox", "0 0 100 40");
     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    let space = 3;
-    for (let i = arrayConverted.length - 1; i >= 0; i--) {
+    let space = 0;
+    for (let i = 0; i < arrayConverted.length; i++) {
+      const result = arrayConverted[i]>0?20 - arrayConverted[i]:20 - arrayConverted[i]
+      const fixed = result.toFixed(2)
       const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-      path.setAttribute("d", "M" + space + " 100 v-" + Math.round(arrayConverted[i]));
+      path.setAttribute("d", "M" + space + ',' + fixed +" V 20");
       // path.setAttribute("fill", "none")
-      path.setAttribute("stroke", "#000000");
-      path.setAttribute("stroke-width", "2");
-      path.setAttribute("data-value", randomData[i]);
+      // path.setAttribute("stroke", "var(--color4)");
+      path.setAttribute("stroke-width", "3");
+      path.setAttribute("width", "4");
+      path.setAttribute("data-value", data[i]);
       svg.appendChild(path);
-      space += 3;
+      space += 4;
     }
 
     main.appendChild(svg);
 
-    const output = randomData[0] + " C";
+    const output = data[0] + " C";
 
     main.addEventListener("mouseover", function (e) {
       if (e.target.tagName === "path") {
@@ -347,7 +438,7 @@
   const classNameVariables = [0, "a", "b", "c", "d", "e", "f"];
   const THEME_CHANGE = arrayHelper.call(classNameVariables);
 
-  const changerClass = (e) => {
+  const changerClass = e => {
     if (e) root.className = classNameVariables[e];
     else root.removeAttribute("class");
   };
@@ -357,15 +448,16 @@
     movable.forEach((e) => {
       if (e.id !== "moves") e.style[pos] = parseInt(e.style[pos]) + val * selected + "px";
     });
-    await delay(1000);
+    await delay(300);
     await setStyles();
   };
 
   // document click events
   root.addEventListener("click", (e) => {
-    if (e.target.id === "gt") THEME_CHANGE.decrement(); // eslint-disable-line
-    if (e.target.id === "lt") THEME_CHANGE.increment(); // eslint-disable-line
-    if (e.target.id === "lt" || e.target.id === "gt") {
+    const target = e.target.id
+    if (target === "gt") THEME_CHANGE.decrement(); // eslint-disable-line
+    if (target === "lt") THEME_CHANGE.increment(); // eslint-disable-line
+    if (target === "lt" || target === "gt") {
       e.preventDefault();
       changerClass(THEME_CHANGE.value);
       //set local storage only when user click
@@ -373,10 +465,10 @@
       setColors();
     }
 
-    if (e.target.id === "left") loops("left", -1);
-    if (e.target.id === "right") loops("left", 1);
-    if (e.target.id === "top") loops("top", -1);
-    if (e.target.id === "bottom") loops("top", 1);
+    if (target === "left") loops("left", -1);
+    if (target === "right") loops("left", 1);
+    if (target === "top") loops("top", -1);
+    if (target === "bottom") loops("top", 1);
   });
 
   const moves = document.getElementById("moves");
@@ -411,8 +503,17 @@
     const compStyles = window.getComputedStyle(root);
     const colors = document.querySelectorAll("#colors input");
     const arrayColors = [];
-    colors.forEach((e, i) => {
-      const compValue = compStyles.getPropertyValue("--color" + i);
+    colors.forEach(async (e, i) => {
+
+      e.parentElement.getElementsByTagName('label')[0].onclick = function(e) {
+        if(e.target.tagName === 'LABEL'){
+        // e.target.textContent = this.value
+        copyToClipboard(this.title);
+        e.preventDefault()
+        }
+      }.bind(e)
+
+      const compValue = await compStyles.getPropertyValue("--color" + i);
       e.value = e.title = compValue;
 
       e.addEventListener("input", async (e) => {
@@ -423,7 +524,7 @@
           })
           .filter(Boolean);
         const styles = styleArray.join(";");
-        localStorage.setItem("custom-theme", styles);
+        await localStorage.setItem("custom-theme", styles);
         styleRoot();
       });
     });
@@ -435,23 +536,25 @@
 
   async function init() {
     const areaText = document.getElementsByTagName("TEXTAREA")[0];
-    styleRoot();
     if (!localStorage.getItem("elementClass")) localStorage.setItem("elementClass", JSON.stringify([14]));
-    areaText.value = localStorage.getItem("textArea") || "Good day. How may I assist you? You have the ability to reposition these blocks by selecting and holding the left corner at your desired location, or double-click to minimize them. Additionally, you can customize the theme, colors, and background image to your liking. You are free to tailor this interface to your preferences. Alternatively, you may opt to close this window and continue with your activities. The decision is entirely yours. Wishing you a splendid day, and a fulfilling life.";
+    areaText.value = localStorage.getItem("textArea") || textAreaDefaults;
     // defaults by injecting to storage then loading string can be changed from localStorage (HTML should be not touched)
-    if (!localStorage.getItem("elementStyles")) localStorage.setItem("elementStyles", "width:100px;height:60px;left:290px;top:10px;,width:180px;height:60px;left:10px;top:10px;,width:100px;height:60px;left:190px;top:10px;,width:80px;height:60px;left:390px;top:10px;,width:80px;height:60px;left:470px;top:10px;,width:140px;height:40px;left:220px;top:450px;,width:190px;height:140px;left:360px;top:430px;,width:220px;height:40px;left:200px;top:170px;,width:220px;height:100px;left:10px;top:70px;,width:130px;height:40px;left:420px;top:170px;,width:190px;height:160px;left:360px;top:270px;,width:190px;height:60px;left:360px;top:210px;,width:320px;height:50px;left:230px;top:120px;,width:320px;height:50px;left:230px;top:70px;,width:20px;height:120px;left:200px;top:450px;,width:160px;height:240px;left:200px;top:210px;,width:140px;height:80px;left:220px;top:490px;,width:190px;height:350px;left:10px;top:220px;,width:160px;height:260px;left:1100px;top:140px;,width:190px;height:50px;left:10px;top:170px;");
+    if (!localStorage.getItem("elementStyles")) localStorage.setItem("elementStyles", blockDefaults);
     // check if there is no data in local storage or check if there time passed 43minutes and load api
     if (setTimeStamp(43) && online) await getAll(api_url);
     await stats(getValueOfStorage.call("statsData"));
-    await startTime();
     await applyStyles();
     await loopElem();
     document.getElementById("today").innerHTML = showDate();
     document.body.style.display = "block";
-    const NUM = parseInt(getValueOfStorage.call("theme")) || random(0, classNameVariables.length);
+    const NUM = parseInt(getValueOfStorage.call("theme")) || 0; //random(0, classNameVariables.length);
     THEME_CHANGE.value = NUM;
     changerClass(NUM);
-
+    styleRoot();
+    if (JSON.parse(localStorage.getItem("theme-lines")) === false) document.body.classList.remove('bglines')
+    const clock = new Clock("clock");
+    clock.startTime();
+    const bgToggle = document.querySelector("#bg-toggle");
     const themeReset = document.querySelector("#custom-theme");
     const bgReset = document.querySelector("#bg-theme");
     const resetAll = document.querySelector("#reset-all");
@@ -475,7 +578,7 @@
       if (inputValue) reader.readAsDataURL(inputValue);
     });
 
-    [resetAll, bgReset, themeReset].forEach((e) =>
+    [resetAll, bgReset, themeReset, bgToggle].forEach((e) =>
       e.addEventListener("click", (e) => {
         root.removeAttribute("style");
         localStorage.removeItem(e.target.id);
@@ -484,6 +587,9 @@
           root.removeAttribute("class");
           localStorage.clear();
           setColors();
+        }
+        if (e.target === bgToggle) {
+          localStorage.setItem('theme-lines',document.body.classList.toggle('bglines'))
         }
         if (e.target === themeReset) {
           setColors();
@@ -504,6 +610,7 @@
   let scalingTarget = null;
   document.addEventListener("mousedown", async (e) => {
     scalingTarget = e.target;
+
     if (scalingTarget.tagName === "TEXTAREA") {
       mousedown = true;
       await delay(300);
@@ -523,7 +630,9 @@
   function mouseEvents(e) {
     const { target } = e;
     // to make opacity .3 like link is visited while not refreshed page
-    if (target.parentElement?.className === "movable" && target.tagName === "A") target.style.opacity = ".5";
+    if (target.parentElement?.className === "movable" && target.tagName === "A") {
+      target.style.opacity = ".5";
+    }
 
     try {
       if (scalingTarget.tagName === "TEXTAREA" && scalingTarget != null) {
