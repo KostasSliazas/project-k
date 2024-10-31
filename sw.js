ea04374ad13a6028e0f77369d618d7876dda8d86
Kostas <kostas.sliazas@gmail.com>
2024-10-31 17:29:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = "project-k-v1";
 const urlsToCache = [
-  "/",  // Cache the root URL (home page)
-  "/index.html",  // Cache the index.html file
-  "/css/main.min.v1.css?v=1",  // Cache the CSS file with versioning
-  "/js/gall7.min.v1.js?v=1",  // Cache the gallery file with versioning
-  "/js/main.min.v1.js?v=1",  // Cache the main JavaScript file with versioning
-  "/js/sw.js?v=1"  // Cache the service worker itself with versioning
+  "/project-k/",  // Cache the root URL (home page)
+  "/project-k/index.html",  // Cache the index.html file
+  "/project-k/css/main.min.v1.css?v=1",  // Cache the CSS file with versioning
+  "/project-k/js/gall7.min.v1.js?v=1",  // Cache the gallery file with versioning
+  "/project-k/js/main.min.v1.js?v=1",  // Cache the main JavaScript file with versioning
+  "/project-k/js/sw.js?v=1"  // Cache the service worker itself with versioning
 ];
 
 self.addEventListener("install", (e) => {
